f9a30b78f06b34c003246cd907b82ce33c2538ee
HarryCandlish <hcnd90@gmail.com>
2019-10-16 11:19:46 +1300

    added home page content

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 import Navigation from './layout/Navigation'
+import Home from './pages/Home'
 
 function App() {
   return (
     <Router>
       <Navigation/>
+      <Route exact path='/' component={Home}/>
     </Router>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,56 @@
+  
+import React, { Component } from "react";
+
+import homeStyles from "../modules/home.module.scss";
+
+import people from "../images/people.jfif";
+import website from "../images/website.jfif";
+
+
+class Home extends Component {
+  render() {
+
+      var hs = homeStyles
+
+    return (
+      <div>
+        <div className={hs.gridContainer}>
+            <div className={hs.overlay}>
+                <h1 className={hs.gridTitleOne}>
+                     How we can help your business.
+                </h1>
+                    <p className={hs.gridParagraphOne}>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                      enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                      nisi ut aliquip ex ea commodo consequat.
+                   </p>
+                <button className={hs.gridButtonOne}>SIGN UP</button>
+            </div>
+            <div>
+                 <img className={hs.image} src={website} alt="website" />
+            </div>
+          </div>
+            <div className={hs.gridContainerTwo}>
+            <div>
+  <img className={hs.imageTwo} src={people} alt="people" />
+</div>
+                <div className={hs.overlayTwo}>
+            <h1 className={hs.gridTitleTwo}>
+                 Are you a junior seeking employment?
+            </h1>
+  <p className={hs.gridParagraphTwo}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat.
+  </p>
+  <button className={hs.gridButtonTwo}>JOIN US</button>
+</div>
+</div>
+</div>
+    );
+  }
+}
+
+export default Home;
